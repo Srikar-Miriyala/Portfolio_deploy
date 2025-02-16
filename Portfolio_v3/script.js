227753fed8003158b705051ea8fa72679bd00e3f
Srikar Miriyala <159043291+Srikar-Miriyala@users.noreply.github.com>
2025-02-16 15:24:08 +0530

    Add files via upload

---
+++ Portfolio_v3/script.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.home', { duration: 2000, origin: 'top', distance: '100px', reset: true });
+ScrollReveal().reveal('.projects', { duration: 2000, origin: 'right', distance: '100px', reset: true });
+ScrollReveal().reveal('.pro', { duration: 2000, origin: 'bottom', distance: '100px', reset: true });
+ScrollReveal().reveal('skills', { duration: 2000, origin: 'right', distance: '100px', reset: true });
+ScrollReveal().reveal('Download-button', { duration: 2000, origin: 'right', distance: '100px', reset: true });
